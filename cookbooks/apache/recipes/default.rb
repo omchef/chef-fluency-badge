5460fbd0e1104b3d124111345e21e7830ab77f32
Ortland <senfroyd@gmail.com>
2017-07-16 06:13:14 +0000

    Fixed node attributes in default file

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,9 +5,9 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #
 
-if node['platoform_family'] == "rhel"
+if node['platform_family'] == "rhel"
 	package = "httpd"
-elseif node['platform_family'] == "debian"
+elsif node['platform_family'] == "debian"
 	package = "apache2" 
 end 
 
@@ -16,7 +16,7 @@ package 'apache2' do
 	action :install
 end
 
-service 'apache' do 
+service 'apache2' do 
 	service_name 'httpd'
 	action [:start, :enable]
 end
